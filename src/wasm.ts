04bd9636a3fc70f5d23b5e61add7fb3d18604d27
Derrick Hammer <derrick@derrickhammer.com>
2023-07-08 04:29:37 -0400

    fix: add ?init query string for vite bundler

--- src/wasm.ts
+++ src/wasm.ts
@@ -10,7 +10,7 @@ export default async function (imports) {
   }
 
   // @ts-ignore
-  let wasm = await import("./wasm/bao.wasm");
+  let wasm = await import("./wasm/bao.wasm?init");
   wasm = wasm.default || wasm;
   return (await wasm(imports)).instance;
 }
